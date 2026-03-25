b3b51aca546919dd8d06ee1f21fe48cda6322aaf
PhanVuLinh <linhphanzzz04@gmail.com>
2026-03-26 01:19:10 +0700

    Làm giao diện FE header

--- public/client/assets/js/script.js
+++ public/client/assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("ok")
+const menuBtn = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+// overlay
+const overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+// open menu
+menuBtn.onclick = () => {
+  mobileMenu.classList.add('active');
+  overlay.classList.add('active');
+};
+
+// close menu
+overlay.onclick = () => {
+  mobileMenu.classList.remove('active');
+  overlay.classList.remove('active');
+};
+
+// dropdown mobile
+document.querySelectorAll('.has-sub').forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('active');
+  });
+});
