79fab16f7eaa8584d8c16124e4bcf2559cfee594
PhanVuLinh <linhphanzzz04@gmail.com>
2026-04-15 12:29:12 +0700

    Hiển thị data section3

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+module.exports.index = async (req, res) => {
+  //section 2
+  const productListSection3 = await Product
+    .find({
+      deleted: false,
+      status: "active",
+    })
+    .sort({
+      position: "desc",
+    })
+    .limit(6);
+  //End section 2
   res.render("client/pages/home.pug", {
     title: "Trang chủ",
+    productListSection3: productListSection3,
   });
-}
+};
--- public/client/assets/js/script.js
+++ public/client/assets/js/script.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const track = document.getElementById('promo-track');
-  const dots = document.querySelectorAll('#promo-dots .dot');
-  
-  if(!track || dots.length === 0) return;
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.getElementById("promo-track");
+  const dots = document.querySelectorAll("#promo-dots .dot");
+
+  if (!track || dots.length === 0) return;
 
   let currentIndex = 0;
   let autoplayInterval;
   const slideTo = (index) => {
     const itemWidth = track.children[0].getBoundingClientRect().width;
-    const gap = 30; 
+    const gap = 30;
     const moveDistance = (itemWidth + gap) * index;
-    
+
     track.style.transform = `translateX(-${moveDistance}px)`;
 
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[index].classList.add('active');
-    
+    dots.forEach((dot) => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+
     currentIndex = index;
   };
 
   dots.forEach((dot) => {
-    dot.addEventListener('click', (e) => {
-      const clickedIndex = parseInt(e.target.getAttribute('data-index'));
+    dot.addEventListener("click", (e) => {
+      const clickedIndex = parseInt(e.target.getAttribute("data-index"));
       slideTo(clickedIndex);
-      
+
       resetAutoplay();
     });
   });
@@ -42,18 +42,16 @@ document.addEventListener('DOMContentLoaded', () => {
   startAutoplay();
 });
 
-
 // Thay đổi ảnh Gallery
 function changeImage(element) {
   const newSrc = element.src;
-  document.getElementById('main-image').src = newSrc;
-  
-  const thumbs = document.querySelectorAll('.gallery-thumbs img');
-  thumbs.forEach(thumb => thumb.classList.remove('active'));
-  
-  element.classList.add('active');
-}
+  document.getElementById("main-image").src = newSrc;
 
+  const thumbs = document.querySelectorAll(".gallery-thumbs img");
+  thumbs.forEach((thumb) => thumb.classList.remove("active"));
+
+  element.classList.add("active");
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   // 1. CHỨC NĂNG MỞ/ĐÓNG MOBILE MENU
@@ -83,21 +81,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 2. CHỨC NĂNG XỔ MENU CON (ACCORDION) TRONG MOBILE MENU
   const menuArrows = document.querySelectorAll(".nav-links .arrow");
-  
+
   menuArrows.forEach((arrow) => {
     arrow.addEventListener("click", function (e) {
-   
       if (window.innerWidth <= 992) {
         e.preventDefault();
-       
+
         const parentLi = this.closest("li");
         const dropdown = parentLi.querySelector(".dropdown");
-        
+
         if (dropdown) {
           dropdown.classList.toggle("active");
-          this.classList.toggle("active"); 
+          this.classList.toggle("active");
         }
       }
     });
   });
 });
+
+// CHỨC NĂNG SLIDER CHO FLASH DEALS
+const flashList = document.querySelector(".flash-products-list");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+if (flashList && prevBtn && nextBtn) {
+  nextBtn.addEventListener("click", () => {
+    const itemWidth =
+      flashList.querySelector(".flash-product-item").offsetWidth + 20;
+    flashList.scrollBy({ left: itemWidth, behavior: "smooth" });
+  });
+
+  prevBtn.addEventListener("click", () => {
+    const itemWidth =
+      flashList.querySelector(".flash-product-item").offsetWidth + 20;
+    flashList.scrollBy({ left: -itemWidth, behavior: "smooth" });
+  });
+}
