98ba1f797e0095635f7c2a4a9523e66c003005e7
PhanVuLinh <linhphanzzz04@gmail.com>
2026-04-19 16:59:17 +0700

    Chỉnh sửa đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -47,8 +47,69 @@ module.exports.list = async (req, res) => {
   });
 };
 
-module.exports.edit = (req, res) => {
-  res.render("admin/pages/order-edit.pug", {
-    title: "Chỉnh sửa đơn hàng",
-  });
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const orderDetail = await Order.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    for (const item of orderDetail.products) {
+      const infoProduct = await Product.findOne({
+        _id: item.product_id,
+        deleted: false,
+      });
+      if (infoProduct) {
+        const priceNewQuantity = item.priceNew * item.quantity;
+        item.priceNewQuantity = priceNewQuantity;
+        item.title = infoProduct.title;
+        item.slug = infoProduct.slug;
+        item.thumbnail = infoProduct.thumbnail;
+      }
+    }
+
+    orderDetail.paymentMethodName = variableCongfig.paymentMethod.find(
+      (item) => item.value === orderDetail.paymentMethod,
+    ).label;
+
+    orderDetail.createdAtFormat = moment(orderDetail.createdAt).format(
+      "HH:mm - DD/MM/YYYY",
+    );
+
+    res.render("admin/pages/order-edit.pug", {
+      title: `Đơn hàng ${orderDetail.orderCode}`,
+      orderDetail: orderDetail,
+      paymentStatus: variableCongfig.paymentStatus,
+      orderStatus: variableCongfig.orderStatus,
+    });
+  } catch (error) {
+    req.flash("error", "Không tồn tài");
+    res.redirect(`/${variableCongfig.pathAdmin}/order/list`);
+  }
+};
+
+module.exports.editPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const order = await Order.findOne({
+      _id: id,
+      deleted: false,
+    });
+    if (!order) {
+      return res.redirect(`/${variableCongfig.pathAdmin}/order/list`);
+    }
+    await Order.updateOne(
+      {
+        _id: id,
+        deleted: false,
+      },
+      req.body,
+    );
+    req.flash("success", "Cập nhật trạng thái đơn hàng thành công");
+    res.redirect(req.get("Referer"));
+  } catch (error) {
+    req.flash("error", "Không tồn tài");
+    res.redirect(`/${variableCongfig.pathAdmin}/order/list`);
+  }
 };
