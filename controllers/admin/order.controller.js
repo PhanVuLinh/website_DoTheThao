3f1ac19f74af8fc0abb075f3d1548cc521175acf
PhanVuLinh <linhphanzzz04@gmail.com>
2026-04-29 22:41:58 +0700

    Trang thùng rác đơn hàng

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,6 +1,7 @@
 const moment = require("moment");
 const Order = require("../../models/order.model");
 const Product = require("../../models/product.model");
+const Account = require("../../models/account.model");
 
 const variableCongfig = require("../../config/variable");
 
@@ -111,5 +112,104 @@ module.exports.editPatch = async (req, res) => {
   } catch (error) {
     req.flash("error", "Không tồn tài");
     res.redirect(`/${variableCongfig.pathAdmin}/order/list`);
+  }
+};
+
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Order.updateOne(
+      { _id: id },
+      {
+        deleted: true,
+        deletedBy: req.account.id,
+        deletedAt: Date.now(),
+      },
+    );
+    req.flash("success", "Xóa đơn hàng thành công");
+    res.redirect(req.get("Referer"));
+  } catch (error) {
+    req.flash("error", "Không tồn tài");
+    res.redirect(`/${variableCongfig.pathAdmin}/order/list`);
+  }
+};
+
+module.exports.trash = async (req, res) => {
+  const find = {
+    deleted: true,
+  };
+
+  const orderList = await Order.find(find).sort({
+    deletedAt: "desc",
+  });
+
+  for (const order of orderList) {
+    for (const item of order.products) {
+      const infoProduct = await Product.findOne({
+        _id: item.product_id,
+        deleted: false,
+      });
+
+      if (infoProduct) {
+        item.title = infoProduct.title;
+        item.thumbnail = infoProduct.thumbnail;
+
+        item.priceNewQuantity = item.priceNew * item.quantity;
+      }
+    }
+    if (order.deletedBy) {
+      const infoAccountDeleted = await Account.findOne({
+        _id: order.deletedBy,
+      });
+      order.deletedByFullName = infoAccountDeleted?.fullName;
+    }
+
+    order.paymentMethodName = variableCongfig.paymentMethod.find(
+      (item) => item.value === order.paymentMethod,
+    )?.label;
+    order.paymentStatusName = variableCongfig.paymentStatus.find(
+      (item) => item.value === order.paymentStatus,
+    )?.label;
+    order.statusName = variableCongfig.orderStatus.find(
+      (item) => item.value === order.status,
+    )?.label;
+
+    order.deletedAtFormat = moment(order.deletedAt).format(
+      "HH:mm - DD/MM/YYYY",
+    );
+  }
+
+  res.render("admin/pages/order-trash.pug", {
+    title: "Thùng rác đơn hàng",
+    orderList,
+  });
+};
+
+module.exports.restore = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Order.updateOne(
+      { _id: id },
+      {
+        deleted: false,
+      },
+    );
+    req.flash("success", "Khôi phục đơn hàng thành công");
+    res.redirect(req.get("Referer"));
+  } catch (error) {
+    req.flash("error", "Không tồn tài");
+    res.redirect(`/${variableCongfig.pathAdmin}/trash`);
+  }
+};
+
+module.exports.deleteDestroy = async (req, res) => {
+  try {
+    const id = req.params.id;
+    await Order.deleteOne({ _id: id });
+    req.flash("success", "Đã xóa vĩnh viễn đơn hàng thành công");
+    res.redirect(req.get("Referer"));
+  } catch (error) {
+    req.flash("error", "Không tồn tài");
+    res.redirect(`/${variableCongfig.pathAdmin}/trash`);
   }
 };
--- routes/admin/order.route.js
+++ routes/admin/order.route.js
@@ -8,4 +8,12 @@ router.get("/edit/:id", orderController.edit);
 
 router.patch("/edit/:id", orderController.editPatch);
 
+router.delete("/delete/:id", orderController.delete);
+
+router.get("/trash", orderController.trash);
+
+router.patch("/restore/:id", orderController.restore);
+
+router.delete("/delete-destroy/:id", orderController.deleteDestroy);
+
 module.exports = router;
